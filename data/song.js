7f487c0547e09d44c2c24ffde127260869c40158
ftaufik.ros <ftaufik.ros@gmail.com>
2023-07-16 21:45:51 +0700

    update dummy data

--- data/song.js
+++ data/song.js
@@ -1,38 +1,58 @@
 const songs = [
     {
-        "title": "Menuju Senja",
-        "artist": "Payung Teduh",
-        "duration": "3:20",
-        "cover": "https://upload.wikimedia.org/wikipedia/id/5/50/Duniabatas.jpg",
-        "released_year": 2010
-    },
-    {
+        "_id": "64b14792db7b9bf3ddf35c18",
         "title": "Suara",
         "artist": "Hijau Daun",
-        "duration": "3:20",
-        "cover": "https://upload.wikimedia.org/wikipedia/id/c/ca/Hijau_daun.jpg",
-        "released_year": 2010
+        "urlSong": "http://localhost:3000/audio/HijauDaun-Suara.mp3",
+        "image": "https://upload.wikimedia.org/wikipedia/id/c/ca/Hijau_daun.jpg",
+        "play_count": 25,
+        "createdAt": "2023-07-14T13:03:14.109Z",
+        "updatedAt": "2023-07-15T05:51:54.170Z",
+        "__v": 0
     },
     {
-        "title": "Mungkinkah",
-        "artist": "Stinky",
-        "duration": "3:20",
-        "cover": "https://upload.wikimedia.org/wikipedia/id/b/be/Stinky_album.jpg",
-        "released_year": 2010
-    },
-    {
+        "_id": "64b1638b678eb75bc0f4ebdf",
         "title": "Jangan Kau Lepas",
         "artist": "Alexa",
-        "duration": "3:20",
-        "cover": "https://upload.wikimedia.org/wikipedia/id/d/d6/Alexa_Album.jpg",
-        "released_year": 2010
+        "urlSong": "http://localhost:3000/audio/Alexa-Jangan_Kau_Lepas.mp3",
+        "image": "https://upload.wikimedia.org/wikipedia/id/d/d6/Alexa_Album.jpg",
+        "play_count": 17,
+        "createdAt": "2023-07-14T15:02:35.469Z",
+        "updatedAt": "2023-07-15T05:41:47.633Z",
+        "__v": 0
     },
     {
+        "_id": "64b1681b816c6cdf10246975",
         "title": "Aishiteru",
         "artist": "Zivilia",
-        "duration": "3:20",
-        "cover": "https://upload.wikimedia.org/wikipedia/id/4/46/Zivilia_Aishiteru.jpg",
-        "released_year": 2010
+        "urlSong": "http://localhost:3000/audio/Zivilia-Aishiteru.mp3",
+        "image": "https://upload.wikimedia.org/wikipedia/id/4/46/Zivilia_Aishiteru.jpg",
+        "play_count": 4,
+        "createdAt": "2023-07-14T15:22:03.212Z",
+        "updatedAt": "2023-07-15T05:44:45.449Z",
+        "__v": 0
+    },
+    {
+        "_id": "64b1684d816c6cdf10246977",
+        "title": "Menuju Senja",
+        "artist": "Payung Teduh",
+        "urlSong": "http://localhost:3000/audio/PayungTeduh-Menuju_Senja.mp3",
+        "image": "https://upload.wikimedia.org/wikipedia/id/5/50/Duniabatas.jpg",
+        "play_count": 13,
+        "createdAt": "2023-07-14T15:22:53.090Z",
+        "updatedAt": "2023-07-15T05:44:59.540Z",
+        "__v": 0
+    },
+    {
+        "_id": "64b16d7727c87990cf6a169d",
+        "title": "Mungkinkah",
+        "artist": "Stinky",
+        "urlSong": "http://localhost:3000/audio/Stinky-Mungkinkah.mp3",
+        "image": "https://upload.wikimedia.org/wikipedia/id/b/be/Stinky_album.jpg",
+        "play_count": 106,
+        "createdAt": "2023-07-14T15:44:55.059Z",
+        "updatedAt": "2023-07-15T05:08:43.041Z",
+        "__v": 0
     }
 ];
 
